7ad6525e8aaa78fb251bab4f2a3051747e81b47c
Amir Khazaie <733amir@gmail.com>
2015-06-29 18:48:25 +0430

    Update judger.py

--- judger.py
+++ judger.py
@@ -4,13 +4,16 @@ __author__ = 'Amir Khazaie 733amir@gmail.com'
 import sys, os
 from filemanager import FileManager
 from cmdarghan import CmdArgHan
-from defaults import *
+import defaults as df
 from compiler import Compiler
 
 class Judger:
     def __init__(self):
         cmdArgHan = CmdArgHan(sys.argv)
         fileManager = FileManager(cmdArgHan.getCmdArg())
-        compiler = Compiler(fileManager.newPaths())
+        data = fileManager.newPaths()
+        compiler = Compiler(data[df.sourceLanguage], data[df.compiler], data[df.source],
+                            os.path.join(fileManager.tempDirPath(), df.tempExeFile))
+        print(compiler.status())
 if __name__ == '__main__':
     Judger()
